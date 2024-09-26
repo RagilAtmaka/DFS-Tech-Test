323d9ecee38dd0cbed820c611697f98d3c2242b7
Ragil <ragillia.atmaka@sirclo.com>
2024-09-26 14:36:38 +0700

    Adding header Locator

--- resources/locators/Login.py
+++ resources/locators/Login.py
@@ -1,8 +1,9 @@
 #Login
-username            = "//input[@id='user-name']"
-password            = "//input[@id='password']"
+InputUserName       = "//input[@id='user-name']"
+InputPassword            = "//input[@id='password']"
 btnLogin            = "button login"
 errorMessage        =   "//div[@class='error-message-container error']"
 closeErrorMessage   = "//button[@class='error-button']//*[name()='svg']"
-brgrMenu            = "/button[@id='react-burger-menu-btn']"
+brgrMenu            = "//button[@id='react-burger-menu-btn']"
 logout              = "//a[@id='logout_sidebar_link']"
+HeaderLogin         = "//div[@id='header_container']"
